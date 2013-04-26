8336f261956f638b03ba8fd2f9044fd0f7eb10e2
gclover <clover.gch@gmail.com>
2013-04-26 09:17:09 +0800

    add fabfile.py

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 ngxcpp-tpl
 ==========
+
+
+/app/conf  luajit  lualib  nginx lua
+/app/lua/m1 m2 m3
+/app/nginx/lua -> /app/lua
--- fabfile.py
+++ fabfile.py
@@ -1,35 +1,45 @@
 import os
 import string
+import glob
 
 from fabric.api import *
-from fabset.openresty import *
-
-
-name = 'blobserve'
-version = '0.1.0' 
-deploydir = '/opt/space/blobserve' 
 
 nginx_pkg = 'http://nginx.org/download/nginx-1.4.0.tar.gz'
 openresty_pkg = 'http://agentzh.org/misc/nginx/ngx_openresty-1.2.4.14.tar.gz'
 
+"""
+#Command('speed_analysis.tex','code/speed.py','python code/speed.py')
+
+#myfile = env.Command('myfile.out', [], 'echo Hello world > $TARGETS')
+#env.AlwaysBuild(myfile)
+#./configure --with-ld-opt="-lstdc++" --add-module=../src/demoj
+
+./configure --with-luajit --prefix=%s >/dev/null' % installdir, 
+		 'make >/dev/null', 'make install >/dev/null'
+"""
+
 
 def deps():
-	
-	run()
+	local('cd deps;wget %s' % nginx_pkg)
+	local('cd deps;wget %s' % openresty_pkg)
 
-def build():
+def install(dest, resty=True, module=True):
 
-def install():
-	run('mkdir -p %s' % deploydir)
-	run('rm -rf %s' % installdir)
-	run('mkdir -p %s' % installdir)
+	luaconf = '--with-luajit' if resty is True else ''
+	moduleconf = ''
+	if module:
+		moduleconf = '--with-ld-opt="-lstdc++" '	
+		modules = glob.glob('src/cpp/*')
+		for dir in modules:
+			moduleconf += ' --add-module=%s' % os.path.abspath(dir)
 
-	install_openresty(openrestydir, openresty_pkg)
+	config_command = './configure %s %s --prefix=%s' % (luaconf, moduleconf, dest)
 
-        put('conf', installdir)
-        put('lua', installdir)
-	run('rm -f %s/nginx/conf/nginx.conf' % openrestydir)
-	run('ln -s %s/conf/nginx.conf %s/nginx/conf/nginx.conf' % (installdir, openrestydir))
-	run('ln -s %s/lua %s/nginx/lua' % (installdir, openrestydir))
-	
+	pkg = 'ngx_openresty' if resty is True else 'nginx'
+	local('cd deps; tar zxvf %s*.tar.gz' % pkg)
+	local('cd deps/%s*; %s; make; make install' % (pkg, config_command))
+
+        local('cp -rf conf %s' % dest)
+        put('cp -rf src/lua %s' % dest)
+	local('ln -s %s/lua %s/nginx/lua' % (dest, dest))
 
